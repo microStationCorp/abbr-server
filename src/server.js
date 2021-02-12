c3ac8382174146331a9f2856d437220f2bd8df81
sherlock <sherlockcreation@gmail.com>
2021-02-12 17:09:42 +0530

    cors option added

--- src/server.js
+++ src/server.js
@@ -8,6 +8,25 @@ require("dotenv").config();
 
 const app = express();
 
+// Set up a whitelist and check against it:
+var whitelist = [
+  "http://localhost:3000",
+  "https://microstationcorp.github.io/abbr-client/",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+//middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+
 //db setup
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -19,16 +38,10 @@ mongoose
   .then(() => console.log("mongoDB server connected"))
   .catch((err) => console.log(err));
 
-  
-  //middleware
-  app.use(cors());
-  app.use(express.json());
-
-  
-  //router
+//router
 app.use("/abbrserver/api", router);
 app.use("/abbrserver/admin", adminRouter);
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`server starts at ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server starts at ${PORT}`));
